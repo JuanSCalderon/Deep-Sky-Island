cce071bd36d74746d311c23c6ab2aa8418230667
JM4r10 <bakyoram@gmail.com>
2024-07-30 23:28:30 -0500

    Añadi PlayerController.cs, CameraFollow.cs
    PlayerController detecta capas, salta
    Camara sigue al jugador a distancia fija

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController _playerController;
+
+    [SerializeField] float _playerSpeed;
+    [SerializeField] float _rotationSpeed;
+    [SerializeField] float jumpForce;
+    private readonly float gravity = 9.8f;
+
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundCheck;
+
+    private Vector3 velocity;
+    [SerializeField] private bool isGrounded;
+
+    void Start()
+    {
+        _playerController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = GroundCheck();
+
+        if (isGrounded)
+        {
+            velocity.y = 0f;
+            Jump();
+            _playerController.Move(velocity * Time.deltaTime);
+        }
+        
+        Movement();
+
+        if (!isGrounded)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+            _playerController.Move(velocity * Time.deltaTime);
+        }
+
+        
+    }
+
+    private void Movement()
+    {
+
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        Vector3 movementInput = new(hInput, 0, vInput);
+        Vector3 movementDirection = movementInput.normalized;
+
+
+        if (movementInput != Vector3.zero)
+        {
+            Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, _rotationSpeed * Time.deltaTime);
+        }
+
+        _playerController.Move(_playerSpeed * Time.deltaTime * movementDirection);
+    }
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * gravity);            
+        }
+    }
+    private bool GroundCheck()
+    {
+        float rayLength = 0.45f;
+        Ray ray = new(groundCheck.position, Vector3.down);
+        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red);
+        return Physics.Raycast(ray, out RaycastHit hit, rayLength, groundMask);
+    }
+}
+
+
+
+
